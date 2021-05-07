da47b030dd33c0004bb7953d05b488c1572b50be
zhangpeng <zhangpeng@1zhen.com>
2021-05-07 17:18:25 +0800

    fix: 修改打包配置

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -2,6 +2,6 @@ const assert = require('assert');
 describe('webpack.base.conf.js test case', () => {
     const baseConfig = require('../../lib/webpack.base.conf');
     it('entry', () => {
-        assert.equal(baseConfig.entry.home, '/Users/zhangpeng/code/frontend/project/learn/webpack/build-webpack/test/smoke/template/src/home/index.js')
+        assert.equal(baseConfig.entry.home.indexOf('/src/home/index.js') > -1, true)
     })
 })
